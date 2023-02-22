bf0bd9742118978b1badf55bc2f9c23cfcef2bd0
Senethlakshan <kmseneth@gmail.com>
2023-02-23 02:42:21 +0530

    Updated

--- src/main/java/com/example/Userauthapp/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Userauthapp;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findByUsername(String username);
-}
